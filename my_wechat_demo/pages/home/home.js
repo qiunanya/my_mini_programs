e01df012e43ddcb5ecebdeea367b4ebdf6a92bbb
qiunanya <l>
2020-12-10 00:18:52 +0800

    数据绑定，事件交互，列表循环，全局刷新，开始/结束刷新方法，底部菜单栏功能，新增服务，我的，分类页面

--- my_wechat_demo/pages/home/home.js
+++ my_wechat_demo/pages/home/home.js
@@ -10,10 +10,49 @@ Page({
             { id: 1, name: '科比', age: '21' },
             { id: 2, name: '科比2', age: '22' },
             { id: 3, name: '科比3', age: '36' }
-        ]
+        ],
+        studentsList: [
+            { id: 1, name: '杜兰特', age: '21' },
+            { id: 2, name: '汤姆森', age: '22' },
+            { id: 3, name: '保罗', age: '36' }
+        ],
+        count:0
         
     },
-
+    /**
+     * @description 点击加 1
+     */
+    clickAddOne: function() {
+        this.setData({students: this.data.studentsList})
+        this.setData({count:this.data.count+1})
+        this.setData({ msg: "Hello World" })
+    },
+    /**
+     * @description 点击减 1
+     */
+    clickReduceOne: function() {
+        this.setData({count:this.data.count - 1})
+        this.setData({ msg: "Hello World" })
+    },
+    /**
+     * @description 开始刷新
+     * @param {*} options 
+     */
+    startRefresh:function name(params) {
+        wx.startPullDownRefresh({
+            complete: (res) => {console.log('开始刷新')},
+          })
+    },
+    /**
+     * @description 结束刷新
+     * @param {*} options 
+     */
+    stopRefresh:function name(params) {
+        wx.stopPullDownRefresh({
+            complete: (res) => {console.log('结束刷新')},
+          })
+    },
+    
   /**
    * 生命周期函数--监听页面加载
    */
@@ -53,7 +92,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('全局刷新了')
   },
 
   /**
